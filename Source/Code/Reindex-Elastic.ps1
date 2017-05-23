8e7031f3001d107c7e6cdde3a122463728a4a6e1
flyingrabbit2600 <ikoretsky@kcura.com>
2017-05-22 20:14:15 -0500

    Added documentation, cleaned up a function.

--- Source/Code/Reindex-Elastic.ps1
+++ Source/Code/Reindex-Elastic.ps1
@@ -1,16 +1,61 @@
-﻿#Data Grid Reindex Script#
+﻿<#
+.SYNOPSIS
+This script will reindex a Relativity Data Grid index to a new shard count.
 
+.DESCRIPTION
+
+An ElasticSearch index shard count cannot be lowered or raised after creation.  This script was devised with Relativity Data Grid in mind.
+Four parameters are required and as of this version there is not support for shield although this will be implemented in a future version.
+
+BACKUP YOUR INDEX BEFORE RUNNING THIS SCRIPT!!!
+
+The scripts full run will:
+
+1.) Create a new index which will be the same name incremented by one as a target for the operation.
+2.) Copy the mappings from the old index to the new target index.
+3.) Reindex the documents from the old index to the new target index.
+4.) Remove the mappings created by default from both indexes.
+5.) Add the mappings from the old index to the new index.
+6.) Delete the old index.
+
+When the operation is finished no additonal work needs to be done.
+
+.PARAMETER nodeName
+This can be any node in the cluster but the client node should be used and barring that one of the data nodes.
+
+.PARAMETER caseArtifactID
+The Relativity case artifact ID includeing the edds prefix.  This will be the each name of the Relativity Case database.
+
+.PARAMETER prefix
+This will either audit or relativity it cannot be any other word.  The prefix relativity will be for text indexes and audit for audit indexes.
+
+.PARAMETER newShardCount
+An integer value which will be your new shard count on the index generated.
+
+.PARAMETER numberofReplicas
+This will be the number of Replicas shards created for the new index.
+
+.PARAMETER shieldState
+shieldState is not yet implemented and will be used to pass the esusers name and password to the cluster.
+#>
+[CmdletBinding()]
 param(
+[Parameter(Mandatory=$true)]
 $nodeName = "cs-mv1-caw",
+[Parameter(Mandatory=$true)]
 $caseArtifactID = "edds1076732",
+[Parameter(Mandatory=$true)]
 $prefix = "audit",
+[Parameter(Mandatory=$true)]
 [int]$newShardCount = '2',
+[Parameter(Mandatory=$true)]
 [int]$numberOfReplicas = '1',
 [bool]$shieldState = $false
 )
 
 $contentType = 'application/json'
 
+#Returns $index the name of the index for reindexing. 
 Function FindIndex {
     #$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $esUsername,$esPassword)))
 
@@ -29,6 +74,7 @@ Function FindIndex {
     }   
 }
 
+#Increments $index and returns $indexNew the name of the target for the reindex operation.
 Function NewIndexName {
 
     $indexStringArray = $index.ToCharArray()
@@ -42,6 +88,7 @@ Function NewIndexName {
     
 }
 
+#Calls ElasticSearch and creates the index the function returns void.
 Function CreateNewIndex {
 
     Try{
@@ -53,16 +100,16 @@ Function CreateNewIndex {
 
 }
 
+#Calls ElasticSearch and gets the mappings of the original index.  Returns $mappings
 Function GetMappingsOldIndex {
-    (Invoke-RestMethod -URI "http://$nodeName`:9200/$index/_mappings" -Method 'GET' -ContentType $contentType -OutFile c:\json.txt)
-
-    $mappings = (Get-Content C:\json.txt).ToString()
+    $mappings = (Invoke-RestMethod -URI "http://$nodeName`:9200/$index/_mappings" -Method 'GET' -ContentType $contentType).ToString()
 
     $mappings = $mappings -replace "{`"$index`":{`"mappings`":"
 
     $script:mappings = $mappings.Substring(0,$mappings.Length-2) 
 }
 
+#Applies the mappings to the new index returns void.
 Function ApplyMappingsToNewIndex {
 
     Try{
@@ -71,6 +118,7 @@ Function ApplyMappingsToNewIndex {
     Catch [System.Net.WebException] {}
 }
 
+#Reindexes to the new index and returns void.  If the operation times the function falls directly into WaitForComplete function.
 Function Reindex {
     Try {
     Invoke-RestMethod -Uri "http://$nodeName`:9200/_reindex" -Method Post -ContentType $contentType -Body "{ ""source"": { ""index"": ""$index"" }, ""dest"": { ""index"": ""$indexNew"" }}" -ErrorAction SilentlyContinue
@@ -78,12 +126,14 @@ Function Reindex {
     Catch [System.Net.WebException] {}
 }
 
+#Calls ElasticSearch and returns $oldDocsCount which is the number of documents in the original index.
 Function GetDocumentCountOldIndex {
 
     $script:oldDocsCount = Invoke-RestMethod -Uri "http://$nodeName`:9200/_cat/indices?v&index=$index" -Method Get -ContentType $contentType | select -Property syncRoot -ExpandProperty syncRoot | select -Property docs.count -ExpandProperty docs.count
 
 }
 
+#Compares the document counts as reindexing process the function part of the WaitForComplete Fuction.  It is also the break point of the Do Until loop in WaitForComplete.
 Function CompareDocumentCounts {
 
     $script:newDocsCount = Invoke-RestMethod -Uri "http://$nodeName`:9200/_cat/indices?v&index=$indexNew" -Method Get -ContentType $contentType | select -Property syncRoot -ExpandProperty syncRoot | select -Property docs.count -ExpandProperty docs.count
@@ -95,6 +145,7 @@ Function CompareDocumentCounts {
     }
 }
 
+#Final Test to see if the document counts match the script will exit if the count is off at this point.
 Function CompareDocumentCountsTest {
 
     $script:oldDocsCount = Invoke-RestMethod -Uri "http://$nodeName`:9200/_cat/indices?v&index=$index" -Method Get -ContentType $contentType | select -Property syncRoot -ExpandProperty syncRoot | select -Property docs.count -ExpandProperty docs.count
@@ -109,6 +160,7 @@ Function CompareDocumentCountsTest {
     }
 }
 
+#Uses CompareDocumentCounts to ensure the script does not proceed until reindexing is completed.
 Function WaitForComplete {
     Do {
     cls
@@ -117,6 +169,7 @@ Function WaitForComplete {
     } Until ($newDocsCount -eq $oldDocsCount)
 }
 
+#Retrieves three return values $readAliasOld, $writeAliasOld, and verifyAliasOld for use by GenerateAliases.
 Function GetOldIndexAlias {
 
     $aliases = Invoke-RestMethod -URI "http://$nodeName`:9200/$index/_alias/*" -Method 'GET' -ContentType $contentType  | select -Property $index -ExpandProperty $index | select -Property aliases
@@ -129,6 +182,7 @@ Function GetOldIndexAlias {
   }
 }
 
+#Removes the Aliases for both old and new indexes, returns void.
 Function RemoveAliases {
 
     $subString = ($index.Substring(0,$index.Length-1))
@@ -136,6 +190,7 @@ Function RemoveAliases {
     Write-Verbose "Removing the Aliases from $index and $indexNew"
 }
 
+#Calls ElasticSearch and applies the alias from the old index to the new index, returns void.
 Function GenerateAliases {
 
     Invoke-RestMethod -URI "http://$nodeName`:9200/$indexNew/_alias/$readAliasOld" -Method Put -ContentType $contentType 
@@ -143,6 +198,7 @@ Function GenerateAliases {
     Invoke-RestMethod -URI "http://$nodeName`:9200/$indexNew/_alias/$verifyAliasOld" -Method Put -ContentType $contentType
 }
 
+#Removes the old index, returns void.
 Function DeleteOldIndex {
 
     CompareDocumentCounts
@@ -150,6 +206,7 @@ Function DeleteOldIndex {
     Write-Host "The old index: $index has been deleted."
 }
 
+#Main Function where the functions in the script are called in order.
 Function RunTime {
     Write-Host "The case ArtifactID of the case to reindex: $caseArtifactID.`n" -ForegroundColor Cyan;
 
